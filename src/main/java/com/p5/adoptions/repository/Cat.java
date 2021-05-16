13f9433a4340fb8ab8d75a0edb03ee277c225be7
TncBogdan <tncbogdan@gmail.com>
2021-05-16 09:39:26 +0300

    connecting with DB

---
+++ src/main/java/com/p5/adoptions/repository/Cat.java
@@ -0,0 +1,44 @@
+package com.p5.adoptions.repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String photo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Cat setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Cat setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public Cat setPhoto(String photo) {
+        this.photo = photo;
+        return this;
+    }
+}
---
+++ src/main/java/com/p5/adoptions/repository/Dog.java
@@ -0,0 +1,44 @@
+package com.p5.adoptions.repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String photo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Dog setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Dog setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public Dog setPhoto(String photo) {
+        this.photo = photo;
+        return this;
+    }
+}
